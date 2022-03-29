9925412d55e0f675cba809dd3fdd820d0c532c0a
vishalshrivastava <vishalshrivastava914419@gmail.com>
2022-03-29 14:35:08 +0530

    Recyclerview

--- app/src/main/java/com/example/tanant_finder/welcome.java
+++ app/src/main/java/com/example/tanant_finder/welcome.java
@@ -11,17 +11,20 @@ public class welcome extends AppCompatActivity {
 
 
      Button next;
+     Button logoutbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         next= findViewById(R.id.next);
+        logoutbtn = findViewById(R.id.logoutbtn);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(welcome.this,Dashboard.class));
             }
         });
+
 
     }
 }
